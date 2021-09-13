5c4c0977538680f99bf1e413bd3cf9b5098c8546
A-Fischer-Slalom <87672853+A-Fischer-Slalom@users.noreply.github.com>
2021-09-13 07:27:08 -0500

    React app updates with additional APIs for UI

--- src/App.js
+++ src/App.js
@@ -1,35 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  ConversationHeader
+} from "@chatscope/chat-ui-kit-react";
 
-class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={apiResponse:""};
+export function App() {
+  
+  const nameEl = '';
+  const initialValue = [];
+  const [allItems, setData] = useState(initialValue);
+
+  const handleSubmit = e => {
+    console.log('A name was submitted: ' + e);
+    sendMessage(e);
   }
+ 
+  useEffect(() => {
+    fetch('https://slacbot-fischer-client.herokuapp.com/get-messages', {
+      mode: 'cors',
+      method: 'post'
+    })
+    .then(response => response.json())
+    .then(response => {
+      setData(response);
+    })
+  }, [allItems]);
 
-  callAPI(){
-    fetch("https://slacbot-fischer-client.herokuapp.com:3000/send-message")
-    .then(res => res.text())
-    .then(res => this.setState({apiResponse: res}));
-  }
-
-  componentWillMount(){
-    this.callAPI();
-  }
-
-
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          
-        </header>
-        <p>{this.state.apiResponse}</p>
+  return (
+    <div className="App">
+      <div style={{ position: "fixed", height: "500px" , width: "350px", bottom: "0", right: "50px" }}>
+        <MainContainer>
+          <ChatContainer>
+            <ConversationHeader>
+                <ConversationHeader.Content>
+                    <span style={{}}>
+                      Chat With an Expert
+                    </span>
+                </ConversationHeader.Content>
+            </ConversationHeader>
+            <MessageList>
+              {allItems.map(item => (
+                <Message
+                  model={{
+                    message: item.text,
+                    sender: item.from,
+                    direction: item.direction,
+                    position: "single"
+                  }}>
+                    <Message.Header sender={item.from} />
+                </Message>
+              ))}
+            </MessageList>
+            <MessageInput placeholder="Type message here" attachButton={false} onSend={handleSubmit}/>
+          </ChatContainer>
+        </MainContainer>
       </div>
-    );
-  }
+    </div>
+    
+  );
+}
+
+function sendMessage(msgParam){
+  let msgToSend = msgParam;
+
+  msgToSend = msgToSend.replace(' ', '&');
+
+  let fetchUrl = 'https://slacbot-fischer-client.herokuapp.com/send-message/'+msgToSend;
+
+  fetch(fetchUrl, {
+    mode: 'cors',
+    method: 'post'
+  })
+  .then(response => response.text())
+  .then(response => {
+    console.log(response);
+  });
 }
 
 export default App;
